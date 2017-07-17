ba35147d65ddaa139f0b1b326c247676a851226b
Luke Woodward <woodward.lucas@gmail.com>
2017-07-16 21:16:04 -0500

    set up compilation and start general layout

--- wp-content/themes/deadpool/front-page.php
+++ wp-content/themes/deadpool/front-page.php
@@ -6,5 +6,260 @@
  */
 
 get_header(); ?>
-
+<section class="section-box scoreboard">
+	<header class="section-box-title">
+		<h3>Scores</h3>
+	</header>
+	<div class="scoreboard-items">
+		<div class="score-item">
+			<h4 class="score-item-title">Episode</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Brian</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Bunny</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Ashley</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Missa</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Jen</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Brandon</h4>
+			<div class="score-item-content">0</div>
+		</div>
+		<div class="score-item">
+			<h4 class="score-item-title">Filanthi</h4>
+			<div class="score-item-content">0</div>
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Brian</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Bunny</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Ashley</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Missa</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Jen</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Filanthi</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
+<section class="section-box">
+	<header class="section-box-title">
+		<h3>Brandon</h3>
+	</header>
+	<div class="characters">
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+		<div class="character">
+			<h4 class="character-name">Char Name</h4>
+			<img src="https://www.fillmurray.com/400/400" class="character-img" />
+		</div>
+	</div>
+</section>
 <?php get_footer();
---
+++ wp-content/themes/deadpool/header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The standard header template file
+ *
+ * @package Deadpool
+ */
+ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<header class="primary-header">
+			<h1 class="main-title">Game of Thrones</h1>
+			<h2 class="main-subtitle">Season Seven Deadpool</h2>
+		</header>
+		<main class="main-content">
